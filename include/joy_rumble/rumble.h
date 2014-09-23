955551591eed0444e80d694d175215cecdf76fb9
Florian Nolden <fn187-github@yahoo.de>
2014-09-23 14:47:18 +0200

    added support for imu messages

--- include/joy_rumble/rumble.h
+++ include/joy_rumble/rumble.h
@@ -22,16 +22,23 @@
 
 class Rumble {
 public:
-	boost::shared_ptr< const sensor_msgs::Imu> *ptr_imu_msg_old;
 	Rumble();
 	void init(ros::NodeHandle &nh);
 	bool open_device();
 	void write_force(uint16_t length = 0x00f0, uint16_t direction = 0x8000, uint16_t weak = 0x0, uint16_t strong = 0xffff );
 	void callback_imu(const sensor_msgs::Imu::ConstPtr &msg );
 	void callback_rumble(const joy_rumble::Rumble_msg::ConstPtr &msg );
+	uint16_t calculate_force(double delta, double min, double max);
 	//bool callback_rumble_msg(sensor_msgs::ImuPtr &ptr );
 	ros::Subscriber sub_rumble;
 	ros::Subscriber sub_imu;
+	double imu_velo_old[3];
+	double imu_acc_old[3];
+	double max_delta_acc;
+	double max_delta_velo;
+	uint16_t min_rumble;
+	double velo_delta[2]; // min, max vibration at delta
+	double acc_delta[2];
 	int ff_fd;
 
 	virtual ~Rumble();
--- src/rumble.cpp
+++ src/rumble.cpp
@@ -12,10 +12,11 @@ Rumble::Rumble() {
 	// TODO Auto-generated constructor stub
 	update_rate = 5;
 	ff_fd = -1;
-	ptr_imu_msg_old = 0;
 	ptr_effect_buffer = 0;
 	nr_simultaneous_effects = 0;
 	node_name = "joy_rumble";
+	max_delta_acc = 0;
+	max_delta_velo = 0;
 	//imu_old = NULL;
 }
 
@@ -23,8 +24,15 @@ void
 Rumble::init(ros::NodeHandle &nh){
 	nh.param<std::string>("event", event, "/dev/input/event6");
 	nh.param<int>("intervall", update_rate, 5);
-	nh.param<std::string>("sub_topic_imu", sub_topic_imu, "/imu_message");
+	nh.param<std::string>("sub_topic_imu", sub_topic_imu, "/imu/data");
 	nh.param<std::string>("sub_topic_rumble", sub_topic_rumble, "/rumble_message");
+	nh.param<double>("velo_delta_min", velo_delta[0], 0.05);
+	nh.param<double>("velo_delta_max", velo_delta[1], 0.15);
+	nh.param<double>("acc_delta_min", acc_delta[0], 0.5);
+	nh.param<double>("acc_delta_max", acc_delta[1], 2.0);
+	int temp_rumble =0;
+	nh.param<int>("rumble_min", temp_rumble,  32768);
+	min_rumble = (uint16_t) temp_rumble;
 
 	sub_imu = nh.subscribe(sub_topic_imu.c_str(), 3, &Rumble::callback_imu, this);
 	sub_rumble = nh.subscribe(sub_topic_rumble.c_str(), 3, &Rumble::callback_rumble, this);
@@ -47,8 +55,21 @@ bool Rumble::open_device(){
 	ptr_effect_buffer = new boost::circular_buffer<input_event>(nr_simultaneous_effects);
 
 	ROS_INFO_NAMED(node_name,"Open device \"%s\" succeeded, %i simultaneous effects supported", event.c_str(), nr_simultaneous_effects);
-	write_force(0x00f0, 0x0, 0x0, 0x23ff);
+	write_force(0x0120, 0x0, 0x0, 0x23ff);
 	return true;
+}
+
+uint16_t
+Rumble::calculate_force(double delta, double min, double max){
+	uint16_t min_rumble = 0x8000;
+	if(delta < min) return 0x0;
+	else if(delta >= max ) return 0xffff;
+	else{
+		uint16_t scale = 0xffff - min_rumble;
+		uint16_t result = min_rumble + (scale / (max - min))*delta;
+		//ROS_INFO_NAMED("joy_rumble","Delta: %f min: %f max %f %i", delta, min, max, result);
+		return result;
+	}
 }
 
 void
@@ -56,11 +77,45 @@ Rumble::callback_imu(const sensor_msgs::Imu::ConstPtr &msg ){
 	//Do something here
 	//
 
-	//ptr->angular_velocity.x
+	double delta[6];
 
-	//imu_old = ptr;
-	ptr_imu_msg_old = 0;
-	ptr_imu_msg_old = new boost::shared_ptr<const sensor_msgs::Imu>(msg);
+	delta[0] = fabs(msg->angular_velocity.x - imu_velo_old[0]);
+	delta[1] = fabs(msg->angular_velocity.y - imu_velo_old[1]);
+	delta[2] = fabs(msg->angular_velocity.z - imu_velo_old[2]);
+	//ROS_INFO_NAMED("joy_rumble","Velx: %f old Velx: %f delta %f", msg->angular_velocity.x, imu_velo_old[0], delta[0]);
+
+	delta[3] = fabs(msg->linear_acceleration.x - imu_acc_old[0]);
+	delta[4] = fabs(msg->linear_acceleration.y - imu_acc_old[1]);
+	delta[5] = fabs(msg->linear_acceleration.z - imu_acc_old[2]);
+
+	uint16_t force[6];
+	uint16_t final = 0x0;
+	force[0] = calculate_force(delta[0],velo_delta[0],velo_delta[1]);
+	force[1] = calculate_force(delta[1],velo_delta[0],velo_delta[1]);
+	force[2] = calculate_force(delta[2],velo_delta[0],velo_delta[1]);
+
+	force[3] = calculate_force(delta[3],acc_delta[0],acc_delta[1]);
+	force[4] = calculate_force(delta[4],acc_delta[0],acc_delta[1]);
+	force[5] = calculate_force(delta[5],acc_delta[0],acc_delta[1]);
+
+	for(int i=0; i<3 ; i++ ){
+		final = max(final, force[i]);
+		final = max(final, force[i+3]);
+		max_delta_velo = fmax(max_delta_velo, delta[i]);
+		max_delta_acc = fmax(max_delta_acc, delta[i+3]);
+	}
+
+	//ROS_INFO_NAMED("joy_rumble","max %i, delta max, velo: %f acc: %f", final, max_delta_velo, max_delta_acc );
+
+	if(final > min_rumble )	write_force(0x0100,0x0,0x0,final);
+
+	imu_velo_old[0] = msg->angular_velocity.x;
+	imu_velo_old[1] = msg->angular_velocity.y;
+	imu_velo_old[2] = msg->angular_velocity.z;
+
+	imu_acc_old[0] = msg->linear_acceleration.x;
+	imu_acc_old[1] = msg->linear_acceleration.y;
+	imu_acc_old[2] = msg->linear_acceleration.z;
 }
 
 void
